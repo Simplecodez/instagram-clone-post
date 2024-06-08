b8b64e62e641a7bc9650241282fb2cbe44d8eee4
Emmanuel Elendu <102396467+Simplecodez@users.noreply.github.com>
2024-06-08 19:35:25 +0100

    Update loadupdata.ts

--- src/seed-data/loadupdata.ts
+++ src/seed-data/loadupdata.ts
@@ -15,7 +15,17 @@ export class Seed {
 
   async saveUsersFromFile() {
     try {
+      //Incase you need to restart the service, please comment the line to prevent db duplicate error because of the emails
+      // Seed the users first, comment out the user seeding, and seed posts to prevent db duplicate errors.
+      // they should be done one after another and commented out before starting the server for test.
        await Promise.all([userSeeds.map((user) => this.userService.create(user))]);
+      // This can be uncommented to add post for each of the users.
+      //  await Promise.all([
+      //   postsSeeds.map((post, i) => {
+      //     console.log(i);
+      //     return this.postService.create(post as any, i + 1);
+      //   })
+      // ]);
       console.log('Users saved successfully.');
     } catch (error) {
       console.error('Error saving users:', error);
